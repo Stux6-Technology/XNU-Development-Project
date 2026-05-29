12bc65a88139accf5655f4518768153fdb067443
Alperen ERKAN <erkanalperen54@gmail.com>
2026-05-29 14:28:08 +0000

    Upload original XNU codes

--- api.py
+++ api.py
@@ -2,19 +2,27 @@ import os
 os.environ['MOESIF_APPLICATION_ID'] = 'eyJhcHAiOiIxMDUxOjg3NCIsInZlciI6IjIuMSIsIm9yZyI6Ijg3OjczMSIsImlhdCI6MTc3NzU5MzYwMH0.R3d7yXUze4eHQAapprsbKuNkpKa2QJUdlI3ZX-soh34'
 
 from moesif_aws_lambda import MoesifLogger
-# ... geri kalan kodun
-moesif_options = {
-    'application_id': 'eyJhcHAiOiIxMDUxOjg3NCIsInZlciI6IjIuMSIsIm9yZyI6Ijg3OjczMSIsImlhdCI6MTc3NzU5MzYwMH0.R3d7yXUze4eHQAapprsbKuNkpKa2QJUdlI3ZX-soh34',
-    'log_body': True # İsteğe bağlı: API gövdelerini de loglamak istersen True yap
-}
+
+moesif_options = {'log_body': True}
 
 @MoesifLogger(moesif_options)
 def lambda_handler(event, context):
     return {
         'statusCode': 200,
-        'isBase64Encoded': False,
-        'headers': {
-            'Content-Type': 'application/json'
-        },
-        'body': '{"msg": "Hello from Lambda!"}' # Body string formatında olmalı
+        'headers': {'Content-Type': 'application/json'},
+        'body': '{"msg": "Hello from Lambda!"}'
     }
+
+# SAHTE EVENT (API Gateway formatında)
+mock_event = {
+    "httpMethod": "GET",
+    "path": "/test-endpoint",
+    "headers": {"Host": "example.com"},
+    "queryStringParameters": None,
+    "body": None,
+    "requestContext": {"requestId": "123"}
+}
+
+if __name__ == "__main__":
+    lambda_handler(mock_event, {})
+    print("İstek Moesif'e gönderildi!")
